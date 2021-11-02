ac9c9368d777a36242a35ee10a3a601ad4c97a31
Doggo <fearlessdoggo21@vivaldi.net>
2021-11-02 12:00:20 -0500

    remove config.h old

--- config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-static const char delim[] = " %% ";
-static const char title[] = " - GridGuess";
-static const char fontpath[] = "font/roboto.ttf";
-
-#endif /* CONFIG_H */
-
