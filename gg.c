82e8c2beec7812455cd066bb5128563616cf1bc7
Doggo <fearlessdoggo21@vivaldi.net>
2021-11-10 21:44:13 -0600

    I DID IT YES THE BUG IS GONE... oh the code is a mess

--- gg.c
+++ gg.c
@@ -60,46 +60,6 @@ main(int argc, const char *argv[])
 	Row *row = NULL;
 	size_t ques = 0;
 	while (sfRenderWindow_isOpen(window) && running) {
-		sfVector2f pos, origin;
-		sfFloatRect bounds;
-		sfRenderWindow_clear(window, sfBlack);
-		if (row != NULL) {
-			sfText_setCharacterSize(text, width / 15);
-			sfText_setPosition(text, (sfVector2f){ width / 2, height / 2 });
-			sfText_setString(text, ques ? row->question : row->answer);
-
-			bounds = sfText_getLocalBounds(text);
-			origin = (sfVector2f){ bounds.width / 2.f , bounds.height / 2.f };
-			sfText_setOrigin(text, origin);
-			sfRenderWindow_drawText(window, text, NULL);
-		} else {
-			pos = (sfVector2f){ width / 12, height / 12 };
-			for (size_t i = 0; i < board.len; ++i) {
-				sfText_setCharacterSize(text, width / 25);
-				sfText_setPosition(text, pos);
-				sfText_setString(text, board.col[i].title);
-
-				bounds = sfText_getLocalBounds(text);
-				origin = (sfVector2f){ bounds.width / 2.f , bounds.height / 2.f };
-				sfText_setOrigin(text, origin);
-				sfRenderWindow_drawText(window, text, NULL);
-
-				for (size_t j = 0; j < board.col[i].len; ++j) {
-					pos.y += height / 6;
-					sfText_setCharacterSize(text, width / 25);
-					sfText_setPosition(text, pos);
-					sfText_setString(text, board.col[i].row[j].value);
-
-					bounds = sfText_getLocalBounds(text);
-					origin = (sfVector2f){ bounds.width / 2.f , bounds.height / 2.f };
-					sfText_setOrigin(text, origin);
-					sfRenderWindow_drawText(window, text, NULL);
-				}
-				pos.x += width / 6, pos.y = height / 12;
-			}
-		}
-		sfRenderWindow_display(window);
-
 		sfEvent event;
 		while (sfRenderWindow_pollEvent(window, &event)) {
 			switch (event.type) {
@@ -124,5 +84,51 @@ main(int argc, const char *argv[])
 				break;
 			}
 		}
+
+		sfVector2u sz = sfRenderWindow_getSize(window);
+		width = sz.x, height = sz.y;
+
+		sfRenderWindow_setView(window, sfView_createFromRect((sfFloatRect){ 0, 0, width, height }));
+
+#define SET_ORIGIN \
+bounds = sfText_getLocalBounds(text); \
+sfText_setOrigin(text, (sfVector2f) {\
+	bounds.left + bounds.width / 2, \
+	bounds.top + bounds.height / 2 \
+});
+
+		sfVector2f pos, origin;
+		sfFloatRect bounds;
+		sfRenderWindow_clear(window, sfBlack);
+		if (row != NULL) {
+			sfText_setCharacterSize(text, width / 15);
+			sfText_setPosition(text, (sfVector2f){ width / 2, height / 2 });
+			sfText_setString(text, ques ? row->question : row->answer);
+
+			SET_ORIGIN
+			sfRenderWindow_drawText(window, text, NULL);
+		} else {
+			pos = (sfVector2f){ width / 12, height / 12 };
+			for (size_t i = 0; i < board.len; ++i) {
+				sfText_setCharacterSize(text, width / 25);
+				sfText_setPosition(text, pos);
+				sfText_setString(text, board.col[i].title);
+
+				SET_ORIGIN
+				sfRenderWindow_drawText(window, text, NULL);
+
+				for (size_t j = 0; j < board.col[i].len; ++j) {
+					pos.y += height / 6;
+					sfText_setCharacterSize(text, width / 25);
+					sfText_setPosition(text, pos);
+					sfText_setString(text, board.col[i].row[j].value);
+
+					SET_ORIGIN
+					sfRenderWindow_drawText(window, text, NULL);
+				}
+				pos.x += width / 6, pos.y = height / 12;
+			}
+		}
+		sfRenderWindow_display(window);
 	}
 }
